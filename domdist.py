2fb6715313396b37762f4da432a617a68b443c49
Napol Rachatasumrit <naplr42@gmail.com>
2016-11-04 16:33:10 -0400

    Add total_diff

--- domdist.py
+++ domdist.py
@@ -1,7 +1,8 @@
 import sys
+import math
 
 
-def initialize_matrix(x, y):
+def _initialize_matrix(x, y):
     mat = []
     for i in range(len(y)+1):
         mat.append([i])
@@ -12,7 +13,7 @@ def initialize_matrix(x, y):
     return mat
 
 
-def get_type(s):
+def _get_type(s):
     if (s[0] == '#'):
         return 'id'
     elif (s[0] == '.'):
@@ -21,14 +22,14 @@ def get_type(s):
         return 'tag'
 
 
-def normalize_li(li):
+def _normalize_li(li):
     """
     Sort the class sections of each tag.
     """
     norm_li = []
     temp_cls_li = []
     for i in range(len(li)):
-        t = get_type(li[i])
+        t = _get_type(li[i])
         if t == 'cls':
             temp_cls_li.append(li[i])
         else:
@@ -42,7 +43,7 @@ def normalize_li(li):
     return norm_li
 
 
-def parse(s):
+def _parse(s):
     s = s.strip()
     li = []
     start_index = 0
@@ -55,18 +56,18 @@ def parse(s):
             start_index = i
 
     li.append(s[start_index:])
-    norm_li = normalize_li(li)
+    norm_li = _normalize_li(li)
 
     return norm_li
         
 
-def calculate_min_score(mat, i, j, x, y):
+def _calculate_min_score(mat, i, j, x, y):
     old_diag_score = mat[i-1][j-1]
     old_right_score = mat[i][j-1]
     old_down_score = mat[i-1][j]
 
-    xtype = get_type(x[j-1])
-    ytype = get_type(y[i-1])
+    xtype = _get_type(x[j-1])
+    ytype = _get_type(y[i-1])
 
     if x[j-1] == y[i-1]:
         new_diag_score = old_diag_score 
@@ -78,45 +79,57 @@ def calculate_min_score(mat, i, j, x, y):
         else:
             new_diag_score = old_diag_score + 2
 
-        prev_xtype = get_type(x[j-2])
+        prev_xtype = _get_type(x[j-2])
         if (prev_xtype == 'tag'):
             new_right_score = old_right_score + 1
         else:
             new_right_score = old_right_score + 0
-            
+
         new_down_score = old_down_score + 1
 
     return min(new_diag_score, new_right_score, new_down_score)
 
 
-def lev(x, y):
-    mat = initialize_matrix(x, y)
+def _lev(x, y):
+    """
+    Use modified Levenshetein distance algorithm to find the minimum edit.
+    """
+    mat = _initialize_matrix(x, y)
 
     for i in range(1, len(y)+1):
         for j in range(1, len(x)+1):
-            min_score = calculate_min_score(mat, i, j, x, y)
+            min_score = _calculate_min_score(mat, i, j, x, y)
             mat[i].append(min_score)
 
     return mat[len(y)][len(x)]
 
 
 def get_edit_distance(init_dom, dest_dorm):
-    return lev(parse(init_dom), parse(dest_dorm))
+    return _lev(_parse(init_dom), _parse(dest_dorm))
 
 
 if __name__ == '__main__':
-    # x = parse('div.green.dotted a#login')
-    # y = parse('a#login div.green.dotted')
+    x = _parse('div.green.dotted a#login')
+    y = _parse('a#login div.green.dotted')
+    print(_lev(x,y))
 
-    # x = parse('div.header.footer a#signup')
-    # y = parse('div.basic.footer.header a#signup')
+    x = _parse('div.header.footer a#signup')
+    y = _parse('div.basic.footer.header a#signup')
+    print(_lev(x,y))
 
-    # x = parse('div.footer.fixed a#signup.blue.btn')
-    # y = parse('div.header li.btn a#signup')
+    x = _parse('div.footer.fixed a#signup.blue.btn')
+    y = _parse('div.header li.btn a#signup')
+    print(_lev(x,y))
 
+    total_diff = 0
     with open(sys.argv[1], 'r') as f:
         lines = f.readlines()
         for i in range(2, len(lines), 3):
             score = get_edit_distance(lines[i-2], lines[i-1])
-            print("{} - {}".format(score, lines[i]))
+            diff = math.fabs(score - int(lines[i]))
+            print("{} - {} == {}".format(score, lines[i], diff))
+
+            total_diff += diff
+
+    print("total diff: {}".format(total_diff))
 
